a3efe256c683ae9fcf1557d0aad53958712419fb
ianrocha <ianrochapg@gmail.com>
2017-07-13 08:37:23 -0300

    Consertando

--- controllers/estadao.js
+++ controllers/estadao.js
@@ -15,7 +15,7 @@ exports.listar = function (req, res) {
 }
 
 exports.criar = function (req, res) {
-  request('http://www.estadao.com.br/rss/ultimas.xml', {encoding:'latin1'}, function (error, response, body){
+  request('http://www.estadao.com.br/rss/ultimas.xml', function (error, response, body){
 
 
     parseString(body, function (err, result) {
