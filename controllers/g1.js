5759fd807f0c7d75929de7b2857079bcd7144da6
Diandra Andrade e Silva <di.andrade90@gmail.com>
2017-07-19 23:40:02 +0000

    primeiro ajuste

--- controllers/g1.js
+++ controllers/g1.js
@@ -3,50 +3,89 @@ var request = require('request');
 var parseString = require('xml2js').parseString;
 
 
-// listar
-exports.listar = function (req, res) {
-  req.db.collection('g1').find().toArray(function(err, result) {
+// listar paises
+exports.listarPaises = function (req, res) {
+
+  req.db.collection('paises').find().toArray(function(err, result) {
     if (err) {
       return console.log(err)
     };
 
-   res.send(result);
-  });
-}
-
-exports.criar = function (req, res) {
-  request('http://pox.globo.com/rss/g1/politica/mensalao/', function (error, response, body){
-
-
-    parseString(body, function (err, result) {
-
-      for(var i in result.rss.channel[0].item){
-        var itemAtual = result.rss.channel[0].item[i];
-
-        var objeto = {
-          titulo: itemAtual.title[0],
-          desc: stripHtml(itemAtual.description[0]),
-          link: itemAtual.link[0],
-          _id: itemAtual.link[0],
-          dataCriacao: new Date()
-        }
-
-        req.db.collection('g1').save(objeto, function(err, result) {
-          if (err) {
-            return res.sendStatus(503);
-          }
-        });
-        // res.send(result.rss.channel[0]);
-
-      }
-
-
-
-      res.sendStatus(201);
-    });
+    res.send(result);
   });
 };
 
-function stripHtml(text) {
-  return text.replace(/<.*?>/g, '');
-}
+// listar journals
+exports.listarJornais = function (req, res) {
+  var nomeJornal = req.params.jornais;
+  req.db.collection('jornal').find().toArray(function(err, result) {
+    if (err) {
+      return console.log(err);
+    };
+
+    res.send(result);
+  });
+};
+
+// listar cada journals
+exports.listarJornal = function (req, res) {
+  var nomeJornal = req.params.jornais;
+  req.db.collection('jornal').findOne({jornal: nomeJornal}).toArray(function(err, result) {
+    if (err) {
+      return console.log(err);
+    };
+
+    res.send(result);
+  });
+};
+
+
+// listar noticiaS
+exports.listarNoticias = function (req, res) {
+  var nomeNoticia = req.params.noticias;
+  req.db.collection('noticias').find().toArray(function(err, result) {
+    if (err) {
+      return console.log(err);
+    };
+
+    res.send(result);
+  });
+};
+
+
+
+
+
+//
+// exports.criarNoticias = function (req, res) {
+//   request('http://pox.globo.com/rss/g1/politica/mensalao/',
+//   function (error, response, body){
+//     parseString(body, function (err, result) {
+//
+//       for(var i in result.rss.channel[0].item){
+//         var itemAtual = result.rss.channel[0].item[i];
+//
+//         var objeto = {
+//           titulo: itemAtual.title[0],
+//           desc: stripHtml(itemAtual.description[0]),
+//           link: itemAtual.link[0],
+//           _id: itemAtual.link[0],
+//           dataCriacao: new Date()
+//         }
+//
+//         req.db.collection('noticias').save(objeto, function(err, result) {
+//           if (err) {
+//             return res.sendStatus(503);
+//           }
+//         });
+//         // res.send(result.rss.channel[0]);
+//
+//       }
+//       res.sendStatus(201);
+//     });
+//   });
+// };
+//
+// function stripHtml(text) {
+//   return text.replace(/<.*?>/g, '');
+// }
--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 var request = require('request');
 var parseString = require('xml2js').parseString;
 var express = require('express');
+var bodyParser = require('body-parser');
 var expressMongoDb = require('express-mongo-db');
 
 var app = express();
 
-// importa controllers
-var G1Controller = require('./controllers/g1.js');
-var FolhaController = require('./controllers/folha.js');
-var EstadaoController = require('./controllers/estadao.js');
-var TnytController = require('./controllers/tnyt.js');
-var CaliforniaController = require('./controllers/california.js');
-var WashController = require('./controllers/washpost.js');
-var ElpaisController = require('./controllers/elpais.js');
-var ElmundoController = require('./controllers/elmundo.js');
-var LavanController = require('./controllers/lavan.js');
+// inicializa o body parser
+app.use(bodyParser.json());
+
+var MasterController = require('./controllers/g1.js');
+
+var FavoritoController = require('./controllers/favoritos.js');
 
 // inicializa mongo e expoe para o express
 app.use(expressMongoDb('mongodb://localhost:27017/compilador'));
@@ -26,30 +23,35 @@ app.use(function(req, res, next) {
   next();
 });
 
-// cria endpoints para funcoes de controllers
+// endpoints para funcoes de controllers
 
-// app.get('/{pais}', G1Controller.listar);
-// app.get('/{pais}/{jornal}', G1Controller.listar);
+app.get('/paises', MasterController.listarPaises);
+app.get('/paises/:jornais', MasterController.listarJornais);
 
-app.get('/brasil/g1', G1Controller.listar);
-app.post('/brasil/g1', G1Controller.criar);
-app.get('/brasil/folha', FolhaController.listar);
-app.post('/brasil/folha', FolhaController.criar);
-app.get('/brasil/estadao', EstadaoController.listar);
-app.post('/brasil/estadao', EstadaoController.criar);
-app.get('/eua/nyt', TnytController.listar);
-app.post('/eua/nyt', TnytController.criar);
-app.get('/eua/california', CaliforniaController.listar);
-app.post('/eua/california', CaliforniaController.criar);
-app.get('/eua/washpost', WashController.listar);
-app.post('/eua/washpost', WashController.criar);
-app.get('/espanha/elpais', ElpaisController.listar);
-app.post('/espanha/elpais', ElpaisController.criar);
-app.get('/espanha/elmundo', ElmundoController.listar);
-app.post('/espanha/elmundo', ElmundoController.criar);
-app.get('/espanha/lavanguardia', LavanController.listar);
-app.post('/espanha/lavanguardia', LavanController.criar);
+// app.get('/brasil/g1', G1Controller.listar);
+// app.post('/brasil/g1', G1Controller.criar);
+// app.get('/brasil/folha', FolhaController.listar);
+// app.post('/brasil/folha', FolhaController.criar);
+// app.get('/brasil/estadao', EstadaoController.listar);
+// app.post('/brasil/estadao', EstadaoController.criar);
+// app.get('/eua/nyt', TnytController.listar);
+// app.post('/eua/nyt', TnytController.criar);
+// app.get('/eua/california', CaliforniaController.listar);
+// app.post('/eua/california', CaliforniaController.criar);
+// app.get('/eua/washpost', WashController.listar);
+// app.post('/eua/washpost', WashController.criar);
+// app.get('/espanha/elpais', ElpaisController.listar);
+// app.post('/espanha/elpais', ElpaisController.criar);
+// app.get('/espanha/elmundo', ElmundoController.listar);
+// app.post('/espanha/elmundo', ElmundoController.criar);
+// app.get('/espanha/lavanguardia', LavanController.listar);
+// app.post('/espanha/lavanguardia', LavanController.criar);
+
+// app.get('/favoritos', FavoritoController.criar);
+// app.post('/favoritos', FavoritoController.incluir);
+// app.update('/favoritos', FavoritoController.atualizar);
+// app.delete('/favoritos', FavoritoController.apagar);
 
 app.listen(3000, "0.0.0.0", function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 })
