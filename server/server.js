a552fcf04da54616f3bdfac3696230e747c428d5
Diandra Andrade e Silva <di.andrade90@gmail.com>
2017-07-31 13:16:46 +0000

    ajuste 30-07

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var MongoClient = require('mongodb').MongoClient;
 var cron = require('node-cron');
 var request = require('request');
 var parseString = require('xml2js').parseString;
+var FeedParser = require('feedparser');
 
 var db;
 var url = 'mongodb://localhost:27017/compilador';
@@ -14,74 +15,112 @@ MongoClient.connect(url, function(err, database) {
 
 function updateRss(pais, jornal, rssUrl) {
   request(rssUrl, function(error, response, body) {
+
     parseString(body, function(err, result) {
       for (var i in result.rss.channel[0].item) {
         var itemAtual = result.rss.channel[0].item[i];
+        console.log(itemAtual);
+
         var objeto = {
           titulo: itemAtual.title,
           desc: stripHtml(itemAtual.description),
           link: getUrl(itemAtual.link),
-          _id: getUrl(itemAtual.link),
           jornal: jornal,
           pais: pais,
           dataCriacao: new Date()
-        }
+        };
+
+        db.collection("noticias").save(objeto, function(err, result) {
+          if (err) {
+            console.log("Erro ao salvar: " + err);
+          } else {
+            console.log("Salvo com sucesso");
+          }
+        })
       }
 
-      db.collection("noticias").save(objeto, function(err, result) {
-        if (err) {
-          console.log("Erro ao salvar: " + err);
-        } else {
-          console.log("Salvo com sucesso");
-        }
-      })
     })
   });
 
-  
+// var req = request('http://www.thestar.com/feeds.articles.business.rss')
+// var feedparser = new FeedParser([options]);
+//
+// req.on('error', function (error) {
+//   // handle any request errors
+// });
+//
+// req.on('response', function (res) {
+//   var stream = this; // `this` is `req`, which is a stream
+//
+//   if (res.statusCode !== 200) {
+//     this.emit('error', new Error('Bad status code'));
+//   }
+//   else {
+//     stream.pipe(feedparser);
+//   }
+// });
+//
+// feedparser.on('error', function (error) {
+//   // always handle errors
+// });
+//
+// feedparser.on('readable', function () {
+//   // This is where the action is!
+//   var stream = this; // `this` is `feedparser`, which is a stream
+//   var meta = this.meta; // the "meta" is always available in the context of the feedparser instance
+//   var item;
+//
+//   while (item = stream.read()) {
+//     console.log( item = item.title || item.description || item.link || item.jornal);
+//
+//   }
+// });
 
 
-}
+  }
 
-// roda a cada 1 minuto
-cron.schedule('* * * * *', function() {
-  console.log("Rodando serviço de busca de RSS");
-  db.collection("paises").find().toArray(function(err, result) {
-    if (err) {
-      console.log("Erro ao buscar paises");
-    } else {
-      var vetorPaises = result;
+  // roda a cada 2 minuto
+var task = cron.schedule('0-1 * * * *', function() {
+    console.log("Rodando serviço de busca de RSS");
+    db.collection("paises").find().toArray(function(err, result) {
+      if (err) {
+        console.log("Erro ao buscar paises");
+      } else {
+        var vetorPaises = result;
 
-      for (var j in vetorPaises) {
-        for (var k in vetorPaises[j].jornais) {
-          console.log("Encontrado pais: " + vetorPaises[j].nome);
-          console.log("Buscando jornal: " + vetorPaises[j].jornais[k].nome_jornal);
-          console.log("RSS encontrado");
+        for (var j in vetorPaises) {
+          for (var k in vetorPaises[j].jornais) {
+            console.log("Encontrado pais: " + vetorPaises[j].nome);
+            console.log("Buscando jornal: " + vetorPaises[j].jornais[k].nome_jornal);
+            console.log("RSS encontrado");
 
-          updateRss(vetorPaises[j].nome, vetorPaises[j].jornais[k].nome_jornal,
-            vetorPaises[j].jornais[k].rssUrl);
+            updateRss(vetorPaises[j].nome, vetorPaises[j].jornais[k].nome_jornal,
+              vetorPaises[j].jornais[k].rssUrl);
+            }
+          }
+
         }
+      });
+
+    });
+
+  task.start();
+
+
+    function stripHtml(text) {
+      if (!text) {
+        return "";
       }
-
+      if (text instanceof Array) {
+        return text[0].replace(/<.*?>/g, '');
+      }
+      console.log(text);
+      return text.replace(/<.*?>/g, '');
     }
-  });
 
-});
-
-function stripHtml(text) {
-  if (!text) {
-    return "";
-  }
-  if (text instanceof Array) {
-    return text[0].replace(/<.*?>/g, '');
-  }
-  console.log(text);
-  return text.replace(/<.*?>/g, '');
-}
-
-function getUrl(url) {
-  if (url instanceof Array) {
-    return url[0];
-  }
-  return url;
-}
+    function getUrl(url) {
+      if (url instanceof Array) {
+        return url[0];
+      }
+      return url;
+    }
